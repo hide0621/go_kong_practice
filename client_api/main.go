bc747afe2e37187863cad39f90d3e2fa531d9fbd
Fujihara_Hideyuki <hi19de72h2so4@gmail.com>
2022-04-18 09:37:52 +0900

    参照先ソースコードに関して不明点を調べて反映

--- client_api/main.go
+++ client_api/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	//各フィールドをjson形式で表示
 	"encoding/json"
 	"net/http"
 )
@@ -12,18 +13,25 @@ type Client struct {
 	Email       string `json:"email"`
 }
 
+//clientsというハンドラ関数
 func clients(w http.ResponseWriter, req *http.Request) {
+	//wからヘッダーを取得、そのヘッダー情報についてのレスポンスの形式を指定（Content-Type：application/json）
 	w.Header().Set("Content-Type", "application/json")
 	user := Client{
 		CompanyName: "John Inc.",
 		Email:       "john@example.com",
 	}
 	var clients []Client
+	//スライス自体を保持する変数に追加したい要素を格納
 	clients = append(clients, user)
+	//json形式で構造体Clientのフィールドを8003番ポートのwebサーバーに表示させる
 	json.NewEncoder(w).Encode(clients)
 }
 
 func main() {
+	//第二引数にハンドラ関数を渡す
 	http.HandleFunc("/clients", clients)
+	//第二引数にはハンドラ型かハンドラ関数、又はnilを渡す
+	//nilの場合はDefaultServeMuxを使う
 	http.ListenAndServe(":8003", nil)
 }
--- user_api/main.go
+++ user_api/main.go
@@ -8,22 +8,30 @@ import (
 )
 
 type User struct {
+	//各フィールドをjson形式で表示
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 }
 
+//usersというハンドラ関数
 func users(w http.ResponseWriter, req *http.Request) {
+	//wからヘッダーを取得、そのヘッダー情報についてのレスポンスの形式を指定（Content-Type：application/json）
 	w.Header().Set("Content-Type", "application/json")
 	user := User{
 		FirstName: "John",
 		LastName:  "Doe",
 	}
 	var users []User
+	//スライス自体を保持する変数に追加したい要素を格納
 	users = append(users, user)
+	//json形式で構造体Userのフィールドを8002番ポートのwebサーバーに表示させる
 	json.NewEncoder(w).Encode(users)
 }
 
 func main() {
+	//第二引数にハンドラ関数を渡す
 	http.HandleFunc("/users", users)
+	//第二引数にはハンドラ型かハンドラ関数、又はnilを渡す
+	//nilの場合はDefaultServeMuxを使う
 	http.ListenAndServe(":8002", nil)
 }
